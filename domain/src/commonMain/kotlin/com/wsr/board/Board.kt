d8f208aa7f0da2b60bd00c3193558f5e9141c22d
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2024-02-12 01:44:28 +0900

    [move] ListExtの内容をBoardに移動

--- domain/src/commonMain/kotlin/com/wsr/board/Board.kt
+++ domain/src/commonMain/kotlin/com/wsr/board/Board.kt
@@ -2,8 +2,8 @@ package com.wsr.board
 
 import com.wsr.Peace
 import com.wsr.board.tile.Tile
-import com.wsr.replace
 import com.wsr.result.ApiResult
+import com.wsr.result.map
 
 class Board private constructor(private val tiles: List<List<Tile>>) {
     fun getTile(row: Int, column: Int): ApiResult<Tile, BoardException> =
@@ -13,7 +13,23 @@ class Board private constructor(private val tiles: List<List<Tile>>) {
             ?.let { tile -> ApiResult.Success(tile) }
             ?: ApiResult.Failure(BoardException.NotInRangeException)
 
-    fun place(peace: Peace, row: Int, column: Int): Board = Board(tiles.replace(row, column) { it.place(peace) })
+    fun place(peace: Peace, row: Int, column: Int): ApiResult<Board, BoardException> =
+        updateTile(row, column) { it.place(peace) }
 
-    fun remove(row: Int, column: Int) = Board(tiles.replace(row, column) { it.remove() })
+    fun remove(row: Int, column: Int): ApiResult<Board, BoardException> =
+        updateTile(row, column) { it.remove() }
+
+    private fun updateTile(row: Int, column: Int, block: (Tile) -> Tile) =
+        tiles.update(row) { col ->
+            col.update(column) { tile -> ApiResult.Success(block(tile)) }
+        }
+            .map { Board(it) }
+
+    private fun <T> List<T>.update(
+        index: Int,
+        block: (T) -> ApiResult<T, BoardException>,
+    ): ApiResult<List<T>, BoardException> =
+        if (index !in 0..size) ApiResult.Failure(BoardException.NotInRangeException)
+        else block(get(index))
+            .map { subList(0, index) + it + subList(index, size) }
 }
